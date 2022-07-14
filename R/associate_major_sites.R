e4eb7d4dda33c6148842db348e585f02b376a566
davidjbaker79 <davidjbaker79@gmail.com>
2022-07-14 14:47:10 +0100

    Update description

--- R/associate_major_sites.R
+++ R/associate_major_sites.R
@@ -1,5 +1,8 @@
 #' Associating point locations sites with allocated sites
 #'
+#' These sites have been manually aggregated where it looked like multiple
+#' designations had been given for the same site.
+#'
 #' @param x A sf object containing Seaquest sites with original (as recorded)
 #' site coordinates.
 #' @param y An sf object containing allocated site coordinates.
