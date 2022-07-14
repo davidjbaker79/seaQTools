691a56c0552c995b82e6f8dd70a0620bc8887147
davidjbaker79 <davidjbaker79@gmail.com>
2022-07-14 16:59:46 +0100

    Update documentation

--- R/south_west_model_grid_1km.R
+++ R/south_west_model_grid_1km.R
@@ -3,13 +3,13 @@
 #' A sf object providing a 1km grid across the ocean surrounding the south west
 #' of England, excluding the land area.
 #'
-#' @name modGrid1km
+#' @name modGrid1kmSw
 #'
 #' @docType data
 #'
-#' @usage data(modGrid1km)
+#' @usage data(modGrid1kmSw)
 #'
 #' @format An object of class \code{"sf"}
 #'
 #' @keywords datasets
-"modGrid1km"
+"modGrid1kmSw"
