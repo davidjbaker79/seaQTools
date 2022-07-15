6807fa758a135c49dad167aa0453994d7cc82ee6
davidjbaker79 <davidjbaker79@gmail.com>
2022-07-15 09:10:37 +0100

    Environmental dataset

---
+++ R/enviro_data_sw_1km.R
@@ -0,0 +1,16 @@
+#' South West England marine environmental data at 1km resolution
+#'
+#' A data.frame containing sea surface temperature (sst), net primary
+#' productivity (npp), terrain ruggedness index (tri), and depth gridded at
+#' 1km resolution.
+#'
+#' @name enviro_data_sw_1km
+#'
+#' @docType data
+#'
+#' @usage data(enviro_data_sw_1km)
+#'
+#' @format AN object of class \code{"sf"}
+#'
+#' @keywords datasets
+"enviro_data_sw_1km"
